6c9cf5a88d157e73d8eace285bdfa9c6500e6d9d
Thiago Silveira <durubt@gmail.com>
2010-11-12 16:13:30 -0200

    Add metapacket to integration test

--- server/tests/integration/dispatcher_handler.rb
+++ server/tests/integration/dispatcher_handler.rb
@@ -1,20 +1,38 @@
 class DispatcherHandlerIntegrationTest < Test::Unit::TestCase
-  def test_integration
+  def setup
     PacketHandler.handle 0x01 do
       "return string"
     end
 
-    dispatcher = PacketDispatcher.new(0x00)
-    PacketHandler.register_handlers(dispatcher)
+    @dispatcher = PacketDispatcher.new(0x00)
+  end
 
-    assert dispatcher.has_handler_for? 0x01
-    assert_equal "return string", dispatcher.handle([0x00, 0x01])
+  def test_integration_normal
+    PacketHandler.register_handlers(@dispatcher)
 
-    PacketHandler.unregister_handlers(dispatcher)
+    assert @dispatcher.has_handler_for? 0x01
+    assert_equal "return string", @dispatcher.handle([0x00, 0x01])
 
-    assert !dispatcher.has_handler_for?(0x01)
+    PacketHandler.unregister_handlers(@dispatcher)
+
+    assert !@dispatcher.has_handler_for?(0x01)
     assert_raise RuntimeError do
-      dispatcher.handle([0x00, 0x01])
+      @dispatcher.handle([0x00, 0x01])
+    end
+  end
+
+  def test_integration_metapacket
+    PacketHandler.register_handlers(@dispatcher)
+    packet = Metapacket.new([0x00, 0x01], mock("Socket"))
+
+    assert @dispatcher.has_handler_for? 0x01
+    assert_equal "return string", @dispatcher.handle(packet)
+
+    PacketHandler.unregister_handlers(@dispatcher)
+
+    assert !@dispatcher.has_handler_for?(0x01)
+    assert_raise RuntimeError do
+      @dispatcher.handle(packet)
     end
   end
 end
