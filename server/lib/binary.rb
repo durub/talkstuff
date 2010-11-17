c3d0603a77f2fb7457bfb013d7ff2c6eb7de6720
Thiago Silveira <durubt@gmail.com>
2010-11-16 23:39:26 -0200

    Add Binary module

---
+++ server/lib/binary.rb
@@ -0,0 +1,41 @@
+module Binary
+  module_function
+
+  def from_binary_string(params, string)
+    string.unpack(params)
+  end
+
+  def to_binary_string(object)
+    return to_binary_from_array (object) if object.kind_of? Array
+    return to_binary_from_fixnum(object) if object.kind_of? Fixnum
+    return to_binary_from_float (object) if object.kind_of? Float
+    return to_binary_from_string(object) if object.kind_of? String
+  end
+
+  def to_binary_from_array(array)
+    binary = ""
+
+    array.each do |element|
+      binary << to_binary_string(element)
+    end
+
+    binary
+  end
+
+  def to_binary_from_fixnum(fixnum)
+    return [fixnum].pack("C") if fixnum - 255 <= 0
+    return [fixnum].pack("S") if fixnum - 65536 < 0
+    [fixnum].pack("L")
+  end
+
+  # TODO: FIXME
+  # This server needs to communicate with javascript
+  # Therefore, we need to verify that the floats are compatible
+  def to_binary_from_float(float)
+    [float].pack("g")
+  end
+
+  def to_binary_from_string(string)
+    string
+  end
+end
---
+++ server/tests/unit/binary.rb
@@ -0,0 +1,35 @@
+class BinaryTest < Test::Unit::TestCase
+  include Binary
+
+  def test_from_string
+    assert_equal [0xff, 0x10], from_binary_string("CC", "\xFF\x10")
+    assert_equal [6.5, "my string"], from_binary_string("gZ*", "@\xD0\x00\x00my string")
+  end
+
+  def test_general
+    assert_equal "\xFF", to_binary_string("\xFF")
+    assert_equal "\xFF", to_binary_string([255])
+    assert_equal "\x00\x80", to_binary_string(32768)
+    assert_equal "@\xD0\x00\x00", to_binary_string(6.5)
+  end
+
+  def test_string
+    assert_equal "\xFF", to_binary_from_string("\xFF")
+    assert_equal "\xFF", to_binary_from_string("\xff")
+  end
+
+  def test_array
+    assert_equal "\xFF", to_binary_from_array([255])
+    assert_equal "@\xD0\x00\x00test string\xDB\x10\x00\x80", to_binary_from_array([6.5, "test string", 0xdb, 0x10, 32768])
+  end
+
+  def test_fixnum
+    assert_equal "\xFF", to_binary_from_fixnum(255)
+    assert_equal "\x00\x80", to_binary_from_fixnum(32768)
+    assert_equal "\xD0\xDD\x06\x00", to_binary_from_fixnum(450000)
+  end
+
+  def test_float
+    assert_equal "@\xD0\x00\x00", to_binary_from_float(6.5)
+  end
+end
