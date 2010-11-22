d1203ecd628ee9334e25d75aeafa83b8e2aa4db1
Thiago Silveira <durubt@gmail.com>
2010-11-22 12:57:28 -0200

    Change PacketAdapter from a "Singleton" to an Object.
    There is still work to do. Everything is in a transition state now. The rest of the code should be adapted to use the instanced PacketAdapter instead of the PacketAdapter class.

--- server/lib/packet_adapter.rb
+++ server/lib/packet_adapter.rb
@@ -1,6 +1,45 @@
 class PacketAdapter
   @@adapter = nil
 
+  def initialize(adapters = [])
+    @adapters = []
+
+    adapters.each do |adapter|
+      add_adapter(adapter)
+    end
+  end
+
+  def adapt_in(data)
+    @adapters.each do |adapter|
+      data = adapter.adapt_in(data)
+    end
+
+    data
+  end
+
+  def adapt_out(data)
+    @adapters.each do |adapter|
+      data = adapter.adapt_out(data)
+    end
+
+    data
+  end
+
+  def add_adapter(adapter)
+    raise "Adapter not supported" unless adapter.respond_to?(:adapt_in) && adapter.respond_to?(:adapt_out)
+    @adapters << adapter
+  end
+
+
+  def remove_adapter(adapter)
+    @adapters.delete adapter
+  end
+
+  def use_raw
+    @adapters = []
+  end
+
+  # Transition methods, while Instanced Packet Adapter isn't fully supported
   def self.adapt_in(data)
     return data unless @@adapter
     @@adapter.adapt_in(data)
--- server/tests/unit/packet_adapter.rb
+++ server/tests/unit/packet_adapter.rb
@@ -39,3 +39,54 @@ class PacketAdapterTest < Test::Unit::TestCase
     end
   end
 end
+
+class InstancedPacketAdapterTest < Test::Unit::TestCase
+  def setup
+    @adapter = PacketAdapter.new
+
+    generic = mock("Generic adapter") do
+      stubs(:adapt_in).returns("generic")
+      stubs(:adapt_out).returns("adapter")
+    end
+
+    @adapter.add_adapter(generic)
+  end
+
+  def test_adapt
+    assert_equal "generic", @adapter.adapt_in("random data")
+    assert_equal "adapter", @adapter.adapt_out("anything")
+
+    second = mock("Second adapter") do
+      stubs(:adapt_in).with("generic").returns("adapti")
+      stubs(:adapt_out).with("adapter").returns("adapto")
+    end
+
+    @adapter.add_adapter(second)
+    assert_equal "adapti", @adapter.adapt_in("in")
+    assert_equal "adapto", @adapter.adapt_out("out")
+  end
+
+  def test_raw
+    @adapter.use_raw
+    assert_equal "test string", @adapter.adapt_in("test string")
+    assert_equal "raw string", @adapter.adapt_out("raw string")
+  end
+
+  def test_not_supported
+    unsupported = mock("Useless adapter")
+    assert_raise RuntimeError do
+      @adapter.add_adapter(unsupported)
+    end
+
+    unsupported.stubs(:adapt_in).returns("string")
+    assert_raise RuntimeError do
+      @adapter.add_adapter(unsupported)
+    end
+
+    unsupported = mock("Useless adapter")
+    unsupported.stubs(:adapt_out).returns("string")
+    assert_raise RuntimeError do
+      @adapter.add_adapter(unsupported)
+    end
+  end
+end
