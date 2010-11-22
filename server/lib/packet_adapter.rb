f938a3f59392fcaae0dddb4a0aa41535304d7dde
Thiago Silveira <durubt@gmail.com>
2010-11-22 19:08:59 -0200

    Remove old PacketAdapter class

--- server/lib/packet_adapter.rb
+++ server/lib/packet_adapter.rb
@@ -38,24 +38,4 @@ class PacketAdapter
   def use_raw
     @adapters = []
   end
-
-  # Transition methods, while Instanced Packet Adapter isn't fully supported
-  def self.adapt_in(data)
-    return data unless @@adapter
-    @@adapter.adapt_in(data)
-  end
-
-  def self.adapt_out(data)
-    return data unless @@adapter
-    @@adapter.adapt_out(data)
-  end
-
-  def self.use_adapter(adapter)
-    raise "Adapter not supported" unless adapter.respond_to?(:adapt_in) && adapter.respond_to?(:adapt_out)
-    @@adapter = adapter
-  end
-
-  def self.use_raw
-    @@adapter = nil
-  end
 end
