ee096afadde65a05c093485e8b73eb0f22eec1cc
Thiago Silveira <durubt@gmail.com>
2010-12-05 16:56:03 -0200

    Add support for payload hases in Metapacket (future JSON and family support)

--- server/lib/metapacket.rb
+++ server/lib/metapacket.rb
@@ -1,17 +1,47 @@
 class Metapacket
-  attr_accessor :payload, :socket
+  attr_accessor :payload, :socket, :protocol_number, :action_number
 
   def initialize(payload = [], socket = nil)
+    if payload.kind_of? Hash
+      @protocol_number = payload[:protocol_number]
+      @action_number = payload[:action_number]
+    else
+      @protocol_number = payload[0]
+      @action_number = payload[1]
+    end
+
     @payload = payload
     @socket = socket
+    @stripped = false
   end
 
   def [](index)
+    if !@stripped
+      return @protocol_number if index == 0
+      return @action_number   if index == 1
+    end
+
     @payload[index]
   end
 
   def []=(index, value)
     @payload[index] = value
+    read_meta
+  end
+
+  def strip_meta_from_payload!
+    if !@stripped
+      if @payload.kind_of? Hash
+        @payload.delete(:protocol_number)
+        @payload.delete(:action_number)
+      else
+        @payload = @payload[2..-1]
+      end
+
+      @stripped = true
+    end
+
+    self
   end
 
   def adapt(adapter)
@@ -22,4 +52,21 @@ class Metapacket
     @payload = adapter.adapt_in(@payload).unpack("C*")
     self
   end
+
+  def clone
+    Metapacket.new(@payload.clone, @socket)
+  end
+
+  private
+    def read_meta
+      return nil if @stripped
+
+      if @payload.kind_of? Hash
+        @protocol_number = @payload[:protocol_number]
+        @action_number   = @payload[:action_number]
+      else
+        @protocol_number = @payload[0]
+        @action_number   = @payload[1]
+      end
+    end
 end
--- server/tests/unit/metapacket.rb
+++ server/tests/unit/metapacket.rb
@@ -14,7 +14,7 @@ class MetapacketTest < Test::Unit::TestCase
     packet[2] = 0xff
 
     assert_equal 0x73, packet[0]
-    assert_equal 0xff, packet[2]
+    assert_equal 0xff, packet.payload[2]
   end
 
   def test_adapt
@@ -24,8 +24,45 @@ class MetapacketTest < Test::Unit::TestCase
 
     packet = Metapacket.new("\xdb\x01")
     assert_equal [0xdb, 0x01], packet.adapt(adapter).payload
+    assert_not_equal [0xdb, 0x01], packet.payload
     assert_not_equal packet, packet.adapt(adapter)
     assert_equal packet, packet.adapt!(adapter)
     assert_equal [0xdb, 0x01], packet.payload
+  end
+
+  def test_special
+    packet = Metapacket.new({ :protocol_number => 0x30, :action_number => 0x60, :anything => "string" })
+
+    assert_equal 0x30, packet.protocol_number
+    assert_equal 0x60, packet.action_number
+    assert_equal 0x30, packet[0]
+    assert_equal 0x60, packet[1]
+  end
+
+  def test_hash
+    socket = mock("Socket")
+    packet = Metapacket.new({ :key => "value", :ponies => "unicorns" }, socket)
+
+    assert_equal "value", packet.payload[:key]
+    assert_equal "unicorns", packet.payload[:ponies]
+    assert_equal "value", packet[:key]
+    assert_equal "unicorns", packet[:ponies]
+  end
+
+  def test_strip
+    packet = Metapacket.new({ :protocol_number => 0x30, :action_number => 0x60, :anything => "string" })
+
+    assert_not_nil packet.payload[:protocol_number]
+    assert_not_nil packet.payload[:action_number]
+
+    packet.strip_meta_from_payload!
+
+    assert_nil packet.payload[:protocol_number]
+    assert_nil packet.payload[:action_number]
+
+    assert_equal 0x30, packet.protocol_number
+    assert_equal 0x60, packet.action_number
+    assert_nil packet[0]
+    assert_nil packet[1]
   end
 end
