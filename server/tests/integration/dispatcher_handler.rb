7b88f692b9307957adfa8bd255a9d312250f6134
Thiago Silveira <durubt@gmail.com>
2010-12-07 03:04:20 -0200

    Add integration test for Hash metapacket

--- server/tests/integration/dispatcher_handler.rb
+++ server/tests/integration/dispatcher_handler.rb
@@ -43,4 +43,23 @@ class DispatcherHandlerIntegrationTest < Test::Unit::TestCase
       @dispatcher.handle(packet)
     end
   end
+
+  def test_integration_metapacket_hash
+    PacketHandler.register_handlers(@dispatcher)
+    socket = mock("Socket")
+    packet = Metapacket.new({:protocol_number => 0x00, :action_number => 0x01, :data => "test"}, socket)
+
+    assert @dispatcher.has_handler_for? 0x01
+    assert_equal "return string", @dispatcher.handle(packet)
+
+    packet[:action_number] = 0x02
+    assert_equal [({:data => "test"}), socket], @dispatcher.handle(packet)
+
+    PacketHandler.unregister_handlers(@dispatcher)
+
+    assert !@dispatcher.has_handler_for?(0x01)
+    assert_raise RuntimeError do
+      @dispatcher.handle(packet)
+    end
+  end
 end
