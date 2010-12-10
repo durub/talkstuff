d4ede6d0cf091482542f857d9203502042e0e3c0
Thiago Silveira <durubt@gmail.com>
2010-12-09 22:37:11 -0200

    Improve SessionHandler

--- server/handlers/session.rb
+++ server/handlers/session.rb
@@ -5,13 +5,19 @@ INVALID_KEY = 0x666
 
 class SessionHandler < PacketHandler
   handle NEW_SESSION do
+    server[:user_list].add_user User.new
+
     answer_with :action_number => ANS_NEW_SESSION, :success => 1, :user_id => 0
   end
 
   handle NEW_ADMIN_SESSION do
     secret_key = payload
+    admin_keys = server[:admin_keys]
 
-    if $admin_keys.include?(secret_key)
+    if !admin_keys.nil? && admin_keys.include?(secret_key)
+      admin = User.new(:admin => true)
+      server[:user_list].add_user admin
+
       answer_with :success => 1, :user_id => 0
     else
       answer_with :success => 0, :error_code => INVALID_KEY
--- server/tests/handlers/session.rb
+++ server/tests/handlers/session.rb
@@ -1,27 +1,32 @@
 class SessionTest < HandlerTest
   def setup
-    $admin_keys = []
+    @data = {:admin_keys => ["test_key"]}
+    @data[:user_list] = mock("User list")
   end
 
   def test_new_session
+    @data[:user_list].expects(:add_user).with do |user|
+      !user.admin?
+    end
+
     should_answer_with :action_number => ANS_NEW_SESSION, :success => 1, :user_id => 0
-    call_handler_for NEW_SESSION
+    call_handler_for NEW_SESSION, [], @data
   end
 
   def test_valid_new_admin_session
-    $admin_keys << "test_key"
+    @data[:user_list].expects(:add_user).with do |user|
+      user.admin?
+    end
 
     should_answer_with :success => 1, :user_id => 0
-    call_handler_for NEW_ADMIN_SESSION, "test_key"
+    call_handler_for NEW_ADMIN_SESSION, "test_key", @data
   end
 
   def test_invalid_admin_session
-    $admin_keys << "test_key"
+    should_answer_with :success => 0, :error_code => INVALID_KEY
+    call_handler_for NEW_ADMIN_SESSION, "wrong_key", @data
 
     should_answer_with :success => 0, :error_code => INVALID_KEY
-    call_handler_for NEW_ADMIN_SESSION, "wrong_key"
-
-    should_answer_with :success => 0, :error_code => INVALID_KEY
-    call_handler_for NEW_ADMIN_SESSION
+    call_handler_for NEW_ADMIN_SESSION, [], @data
   end
 end
