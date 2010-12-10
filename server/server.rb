7ed3fe5b225646dccaaa46cf9e953fd01b93eab7
Thiago Silveira <durubt@gmail.com>
2010-12-09 22:37:50 -0200

    Use the TalkServer class in server.rb

--- server/server.rb
+++ server/server.rb
@@ -5,46 +5,11 @@ require_relative 'lib/loader.rb'
 # Load all the code
 Loader.load_lib
 
-# Initialize dispatcher
-$dispatcher = PacketDispatcher.new(0xdb)
-
-# Load and register handlers
-Loader.load_handlers($dispatcher)
-
-# Adapter
-$adapter = PacketAdapter.new
-$adapter.add_adapter(Base64Adapter)
-
-# Debug
-if $DEBUG || ARGV[0] == "debug"
-  $RELOAD = true
-end
+# Set $DEVELOPMENT to always true currently
+$DEVELOPMENT = true
 
 # Start server
 EventMachine.run do
-  EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080, :debug => true) do |ws|
-    ws.onopen do
-    end
-
-    ws.onmessage do |message|
-      # Reload all code if debugging
-      if $RELOAD
-        $dispatcher.instance_eval { @handlers = {} } # some evil metaprogramming to clean up handlers
-
-        Loader.load_lib
-        Loader.load_handlers($dispatcher)
-        $adapter.use_raw
-        $adapter.add_adapter(Base64Adapter)
-      end
-
-      begin
-        $dispatcher.handle(Metapacket.new(message, ws).adapt($adapter))
-      rescue
-        print "handle exception: ", $!, "\n"
-      end
-    end
-
-    ws.onclose do
-    end
-  end
+  server = TalkServer.new
+  server.start
 end
