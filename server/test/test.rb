79a690be5af02d272804aab323b072978d2f1e71
Thiago Silveira <durubt@gmail.com>
2010-12-10 07:21:41 -0200

    Fix recursive require_relative calls

--- server/test/test.rb
+++ server/test/test.rb
@@ -2,6 +2,6 @@ require 'test/unit'
 
 Dir.chdir(File.dirname(__FILE__)) do
   Dir.glob("*.rb").each do |file|
-    require_relative file
+    require_relative file unless file == "test.rb"
   end
 end
